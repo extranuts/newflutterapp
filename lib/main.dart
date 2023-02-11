63bf7f3f7aafbe58f159eaee2443b62b81bf0ccd
HOME-PC\Insane <valeev.timur.v@gmail.com>
2023-02-11 20:10:00 +0300

    Add Text style widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Flutter tutorial .... ",
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Flutter tutor",
-          style: TextStyle(fontSize: 33),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
+void main() => runApp(FlutterTutorialApp());
+
+class FlutterTutorialApp extends StatelessWidget {
+  const FlutterTutorialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter tutorial .... ",
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Flutter tutor",
+              style: TextStyle(fontSize: 33),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey,
+          ),
+          body: RichText(
+              text: const TextSpan(
+                  style: TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.blueGrey,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: "Karla"
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: "Hello, "),
+                    TextSpan(
+                        style: TextStyle(color: Colors.red),
+                        children: <TextSpan> [
+                          TextSpan(text: "Brave ",
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.yellowAccent,
+                          )
+                          ),
+                          TextSpan(text: "New  ",
+                            style: TextStyle(
+                              color: Colors.black,
+                            )
+                          ),
+                          TextSpan(text: "World ",
+                          style: TextStyle(
+                            color: Colors.indigo,
+                            decoration: TextDecoration.underline,
+                          )),
+                        ],
+                    ),
+                    TextSpan(text: " !"),
+                  ]
+              ),
       ),
-      body: const Text("Hello world",
-          style: TextStyle(
-              fontSize: 32.0,
-              color: Colors.blueGrey,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Text("Add"),
       ),
       backgroundColor: Colors.grey[900],
-    ),
-  ));
+    ),);
+  }
 }
